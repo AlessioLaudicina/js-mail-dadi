33440ad54d20ec1febd9e16691610df2e2e26c23
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-27 16:15:31 +0100

    dadi.js

--- dadi/main.js
+++ dadi/main.js
@@ -5,19 +5,29 @@
 
 let numeroUtente = Math.floor(Math.random() * 6) +1;
 console.log(numeroUtente)
+document.getElementById("computer-number").innerHTML = numeroUtente;
+
 
 
 let numeroComputer = Math.floor(Math.random() * 6) +1;
 console.log(numeroComputer)
+document.getElementById("utente-number").innerHTML = numeroUtente;
+
 
 
 if(numeroComputer > numeroUtente){
     console.log("Vittoria Computer")
+    document.getElementById("risultato").innerHTML = "Vittoria Computer";
+
 } else if(numeroComputer < numeroUtente){
     console.log("Vittoria Utente")
+    document.getElementById("risultato").innerHTML = "Vittoria Utente";
+
 
     
 } else{
     console.log("Pareggio")
+    document.getElementById("risultato").innerHTML = "Pareggio";
+
 
 }
--- email/main.js
+++ email/main.js
@@ -2,7 +2,7 @@
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
-const email = ['alessio.laudicina@gmail.com', 'roberto.laudicina@gmail.com', 'aldo.laudicina@gmail.com'];
+const email = ["alessio.laudicina@gmail.com", "roberto.laudicina@gmail.com", "aldo.laudicina@gmail.com"];
 console.log(email);
 
 let emailUtente = prompt("Inserisci la tua email")
@@ -13,7 +13,10 @@ console.log(emailUtente)
 
 if(emailUtente == email){
     console.log("Accesso approvato")
+    document.getElementById("accesso").innerHTML = "Accesso approvato";
 }
 else{
     console.log("Accesso negato")
+    document.getElementById("accesso").innerHTML = "Accesso negato";
+
 }
