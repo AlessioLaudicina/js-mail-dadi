475d990581952b0ca03193a0b44e2eee7de07a50
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-27 14:49:41 +0100

    prompt email

---
+++ email/main.js
@@ -0,0 +1,5 @@
+const email = ['alessio.laudicina@gmail.com', 'roberto.laudicina@gmail.com', 'aldo.laudicina@gmail.com'];
+
+const emailUtente = prompt("Inserisci la tua email")
+console.log(emailUtente)
+
