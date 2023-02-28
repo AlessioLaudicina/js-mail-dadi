bd26b07bfdbafa4669eb47b3fcd7acf08d29200d
Alessio Laudicina <alessiolaudicina1@gmail.com>
2023-02-28 10:07:59 +0100

    email accesso approvato

--- email/main.js
+++ email/main.js
@@ -2,23 +2,32 @@
 controlla che sia nella lista di chi può accedere,
 stampa un messaggio appropriato sull’esito del controllo. */
 
-const email = ["alessio.laudicina@gmail.com", "roberto.laudicina@gmail.com", "aldo.laudicina@gmail.com"];
+const email = ['alessio.laudicina@gmail.com', 'roberto.laudicina@gmail.com', 'aldo.laudicina@gmail.com'];
 console.log(email);
 
-let emailUtente = prompt("Inserisci la tua email")
-console.log(emailUtente)
+let emailUtente = prompt("Inserisci la tua email");
+console.log(emailUtente);
+
+let accesso = false;
 
 for(let i = 0; i < email.length; i++ ){
-    if(emailUtente == email[i]){
-        console.log("Accesso approvato")
-        document.getElementById("accesso").innerHTML = "Accesso approvato";
-    }
-    else{
-        console.log("Accesso negato")
-        document.getElementById("accesso").innerHTML = "Accesso negato";
-    
-    }
+    const emailCorrente = email[i];
 
+    if(emailUtente == email[i]){
+        accesso = true;
+        
+    }
+}
+
+if(accesso){
+    console.log("Accesso approvato")
+    document.getElementById("accesso").innerHTML = "Accesso approvato";
+
+} else{
+    console.log("Accesso negato")
+    document.getElementById("accesso").innerHTML = "Accesso negato";
+
+ 
 }
 
 
